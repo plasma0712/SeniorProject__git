5403e748c79c7fe643603c6811acf5577fc1dd71
plasma0712 <plasma0712@naver.com>
2018-10-09 21:59:05 +0900

    ---------------------------

--- Assets/04 Script/01.Intro/Tutorial/ButtonSetActive.cs
+++ Assets/04 Script/01.Intro/Tutorial/ButtonSetActive.cs
@@ -4,27 +4,17 @@ using UnityEngine;
 
 public class ButtonSetActive : Singleton <ButtonSetActive>
 {
-    //public GameObject Before;
     public GameObject Next;
     public GameObject Finish;
 
     public void Update()
     {
-       //if(TutorialText.Instance.TextNumber>0 && TutorialText.Instance.TextNumber<XMLMapSettingTutorial.Instance.MapSettingTutorialLength())
-       //{
-       //    Before.gameObject.SetActive(true);
-       //}
-       //else if(TutorialText.Instance.TextNumber<=0)
-       //{
-       //    Before.gameObject.SetActive(false);
-       //}
 
-        if(TutorialText.Instance.TextNumber==XMLMapSettingTutorial.Instance.MapSettingTutorialLength()-1)
-        {
-            //Before.gameObject.SetActive(false);
-            Next.gameObject.SetActive(false);
-            Finish.gameObject.SetActive(true);
-            TutorialText.Instance.gameObject.SetActive(true);
-        }
+        //if(TutorialText.Instance.TextNumber==XMLMapSettingTutorial.Instance.MapSettingTutorialLength()-1)
+        //{
+        //    Next.gameObject.SetActive(false);
+        //    Finish.gameObject.SetActive(true);
+        //    TutorialText.Instance.gameObject.SetActive(true);
+        //}
     }
 }
